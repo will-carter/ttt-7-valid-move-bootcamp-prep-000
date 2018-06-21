2589696fc55c2189edddb202354ee13d45dd2762
Will Carter <willmckenna64@gmail.com>
2018-06-21 21:30:33 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?
-  
+  if input < 10
+    return true
+  else
+    return false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
