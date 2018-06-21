e4b728db5c2affef8685e55c2df6e9f07298caae
Will Carter <willmckenna64@gmail.com>
2018-06-21 21:34:42 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 # code your #valid_move? method here
-def valid_move?(index, board)
+def valid_move?(board, index)
   if index.to_i < 9 && board[index] == "X" && board[index] == "O"
     return true
   else
