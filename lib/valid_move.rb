8e30f8c2d76811cd24718fd92f1fe80a43bcb9a2
Will Carter <willmckenna64@gmail.com>
2018-06-21 21:43:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,6 @@ def valid_move?(board, index)
     else
       return false
     end
-  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
